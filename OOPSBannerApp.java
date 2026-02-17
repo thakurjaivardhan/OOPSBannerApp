dbd599693426406842e816df99b00c108203aa22
thakurjaivardhan <thakurjaivardhan@gmail.com>
2026-02-17 11:24:37 +0530

    Initial project setup with UC1 Print OOPS

---
+++ OOPSBannerApp.java
@@ -0,0 +1,15 @@
+/**
+ * OOPSBannerApp UC1 - OOPS Banner Display Application
+ * 
+ * This calss demonstrates A SIMPLE Java application that displays the
+ * OOPS acronym to the console.
+ * 
+ * @author Jaivardhan Thakur
+ * @version 1.0
+ */
+public class OOPSBannerApp {
+
+    public static void main(String[] args){
+        System.out.println("OOPS");
+    }
+}
